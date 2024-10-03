b35bec2eda3de3d0d0e3b7857815b6ea60eaee96
Agustin Sandoval Becerra <guty.boo69@gmail.com>
2024-10-03 11:35:25 -0600

    parseo de stauts

--- back/api/src/common/helpers/parsers/petitionParser.ts
+++ back/api/src/common/helpers/parsers/petitionParser.ts
@@ -1,12 +1,13 @@
 import { RequestPetition } from 'src/vendor/requests/entities/requestPetition.entity';
 import { getDaysBetwen, toUSADate } from '../reportsHelpers';
+import { StatusOrder } from 'src/common/glob/status';
 
 export function exportRejectedPetitionParserMap(items: RequestPetition[]) {
   return items.map((item) => ({
     id: item.id,
     proyectType: item.proyectType.name,
     client: `${item.firstName} ${item.paternalName} ${item.maternalName}`,
-    status: item.status,
+    status: parsestatus(item.status),
     vendor: `${item.vendor?.firstName ?? ''} ${
       item.vendor?.paternalName ?? ''
     } ${item.vendor?.maternalName ?? ''}`,
@@ -21,4 +22,37 @@ export function exportRejectedPetitionParserMap(items: RequestPetition[]) {
     activeDays: getDaysBetwen(item.createdAt, new Date()),
     observations: item.observations,
   }));
+}
+
+export function parsestatus(status: number) {
+  console.log(status === StatusOrder.assigned);
+  if (status === StatusOrder.assigned) {
+    return 'Asignada para inspección';
+  }
+  if (status === StatusOrder.observed) {
+    return 'Observada';
+  }
+  if (status === StatusOrder.inspectionAproved) {
+    return 'Inspección aprobada';
+  }
+  if (status === StatusOrder.instalationAssigned) {
+    return 'Asignada para instalación';
+  }
+  if (status === StatusOrder.instalationReassigned) {
+    return 'Reasignada para instalación';
+  }
+  if (status === StatusOrder.interrnalInspection) {
+    return 'Solicitud de inspección interna';
+  }
+  if (status === StatusOrder.done) {
+    return 'Solicitud finalizada';
+  }
+  if (status === StatusOrder.store) {
+    return 'Solicitud archivada';
+  }
+  if (status === StatusOrder.rejected) {
+    return 'Inspección rechazada';
+  }
+
+  return 'Uknown';
 }
